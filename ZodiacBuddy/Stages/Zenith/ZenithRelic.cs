4b83d02121ad4b567fa72569bf990215d762f8e7
RoseOfficial <christopherscottkeller@gmail.com>
2025-07-16 17:58:41 -0400

    Update ZenithRelic with Correct Weapon IDs
    Replace placeholder IDs for Zenith weapons with the correct values in the ZenithRelic class. Adjust comments to clarify the purpose of base relics and remove outdated TODO notes.

--- ZodiacBuddy/Stages/Zenith/ZenithRelic.cs
+++ ZodiacBuddy/Stages/Zenith/ZenithRelic.cs
@@ -12,22 +12,21 @@ public static class ZenithRelic {
     /// List of Zodiac Zenith weapons and base relics (for testing).
     /// </summary>
     public static readonly Dictionary<uint, string> Items = new() {
-        // Zenith weapons (placeholder IDs - need to be updated with correct values)
-        { 6565, GetItemName(6565) }, // Curtana Zenith
-        { 6566, GetItemName(6566) }, // Sphairai Zenith
-        { 6567, GetItemName(6567) }, // Bravura Zenith
-        { 6568, GetItemName(6568) }, // Gae Bolg Zenith
-        { 6569, GetItemName(6569) }, // Artemis Bow Zenith
-        { 6570, GetItemName(6570) }, // Thyrse Zenith
-        { 6571, GetItemName(6571) }, // Stardust Rod Zenith
-        { 6572, GetItemName(6572) }, // The Veil of Wiyu Zenith
-        { 6573, GetItemName(6573) }, // Omnilex Zenith
-        { 6574, GetItemName(6574) }, // Holy Shield Zenith
-        { 6575, GetItemName(6575) }, // Yoshimitsu Zenith
+        // Zenith weapons (correct IDs)
+        { 6257, GetItemName(6257) }, // Curtana Zenith
+        { 6258, GetItemName(6258) }, // Sphairai Zenith
+        { 6259, GetItemName(6259) }, // Bravura Zenith
+        { 6260, GetItemName(6260) }, // Gae Bolg Zenith
+        { 6261, GetItemName(6261) }, // Artemis Bow Zenith
+        { 6262, GetItemName(6262) }, // Thyrus Zenith
+        { 6263, GetItemName(6263) }, // Stardust Rod Zenith
+        { 6264, GetItemName(6264) }, // The Veil of Wiyu Zenith
+        { 6265, GetItemName(6265) }, // Omnilex Zenith
+        { 6266, GetItemName(6266) }, // Holy Shield Zenith
+        { 9250, GetItemName(9250) }, // Yoshimitsu Zenith
         
-        // Base relics (for testing purposes - will show Zenith info for base weapons)
-        // TODO: Remove these once correct Zenith IDs are found
-        { 2052, GetItemName(2052) }, // Thyrus (base relic for testing) - CORRECT ID
+        // Base relics (show Zenith info when base weapons are equipped)
+        { 2052, GetItemName(2052) }, // Thyrus (base relic)
         { 2046, GetItemName(2046) }, // Curtana (base relic)
         { 2047, GetItemName(2047) }, // Sphairai (base relic)
         { 2048, GetItemName(2048) }, // Bravura (base relic)
